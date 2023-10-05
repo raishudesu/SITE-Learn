78d764ce5fbfeb621d6a7dedc0e82a006a915b96
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-05 22:50:03 +0800

    update

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@/models/User/user";
+import connectDB from "@/lib/mongodb";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { name, email, pwd, confirmPwd } = await req.json();
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+
+    await connectDB();
+
+    const user = await User.findOne({ email });
+    if (user)
+      return NextResponse.json({
+        success: false,
+        status: 400,
+        msg: "User already exists",
+      });
+
+    if (pwd !== confirmPwd) {
+      return NextResponse.json({
+        success: false,
+        status: 400,
+        msg: "Passwords do not match",
+      });
+    }
+
+    await User.create({ name, email, password: hashedPwd });
+
+    return NextResponse.json({
+      success: true,
+      status: 201,
+      msg: "User registered",
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      status: 500,
+      msg: "Internal server error",
+    });
+  }
+};
