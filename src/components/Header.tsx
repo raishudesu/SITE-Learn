3ec7537c34f293e43d02f4385ef3ecd01c3ba0be
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-07 01:12:55 +0800

    updated auth buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 import React from "react";
 import NavMenu from "./NavMenu";
 import { ModeToggle } from "./ModeToggle";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { status } = useSession();
+
+  const hideBtns = () => {
+    if (
+      pathname === "/signin" ||
+      pathname === "/signup" ||
+      status === "authenticated"
+    ) {
+      return "hidden";
+    }
+    if (pathname === "/") return "flex";
+  };
   return (
     <div className="w-full flex justify-center shadow-md">
       <header className="w-full max-w-screen-2xl flex justify-evenly items-center h-16">
@@ -12,9 +31,14 @@ const Header = () => {
         </h1>
         <NavMenu />
         <div className="flex gap-6">
-          <div className="flex gap-2">
-            <Button>Sign in</Button>
-            <Button variant={"outline"}>Sign up</Button>
+          <div className={`${hideBtns()} flex gap-2 `}>
+            <Button onClick={() => router.replace("signin")}>Sign in</Button>
+            <Button
+              onClick={() => router.replace("signup")}
+              variant={"outline"}
+            >
+              Sign up
+            </Button>
           </div>
           <ModeToggle />
         </div>
