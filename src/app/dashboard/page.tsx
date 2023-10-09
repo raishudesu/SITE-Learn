0aba4b2765d02309ef1a1e0d74999958b6399129
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-10 01:20:51 +0800

    added dashboard under development

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,24 @@
 import { Metadata } from "next";
+import Construction from "../../assets/Construction.svg";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
 const Dashboard = async () => {
-  return <section className="h-screen">You are authenticated</section>;
+  return (
+    <section className="min-h-screen flex justify-center">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl font-bold">Currently under development.</h1>
+        <Image
+          src={Construction}
+          alt="construction"
+          className="max-w-[500px]"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
