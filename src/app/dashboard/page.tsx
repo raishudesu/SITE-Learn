4def98add1191374b41f722e5d81b880af363f08
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-10 01:22:05 +0800

    updated screen size

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,13 +8,15 @@ export const metadata: Metadata = {
 
 const Dashboard = async () => {
   return (
-    <section className="min-h-screen flex justify-center">
+    <section className="min-h-[80vh] flex justify-center">
       <div className="flex flex-col items-center justify-center gap-6">
-        <h1 className="text-3xl font-bold">Currently under development.</h1>
+        <h1 className="text-3xl text-center font-bold">
+          Currently under development.
+        </h1>
         <Image
           src={Construction}
           alt="construction"
-          className="max-w-[500px]"
+          className="w-full max-w-[500px]"
         />
       </div>
     </section>
