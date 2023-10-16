0b24162334fc49afa3608edba804997d618fe401
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-17 01:28:25 +0800

    updated pdf viewer

--- src/app/materials/view/PdfViewer.tsx
+++ src/app/materials/view/PdfViewer.tsx
@@ -6,7 +6,7 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 const PdfViewer = ({ url }: { url: string }) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
-    <div className="h-screen w-screen py-3 px-16">
+    <div className="h-screen w-screen py-3 px-3 md:px-16">
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
         <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
       </Worker>
