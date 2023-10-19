db18cd9c6814c4e06e52f3e9287c2be7a89c3f95
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-19 13:28:15 +0800

    updated meta og:image property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <meta property="og:image" content="https://i.imgur.com/HElfXYz.png" />
       <link rel="icon" type="image/svg+xml" href="../assets/Logo.svg" />
       <body className={inter.className}>
         <ThemeProvider
